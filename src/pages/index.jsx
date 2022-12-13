f355e5354d7693e850caea3c217d3bcaf7f01235
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-13 13:21:15 -0300

    add SS cin getStaticProps

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,25 @@
-import Head from 'next/head';
-import {Home } from '../templates/Home';
+import P from 'prop-types';
+import { Home } from '../templates/Home';
+import config from '../config';
+import { mapData } from '../api/map-data';
 
-export default function Index() {
-  return <Home />;
+export default function Index({ data = null }) {
+  return <Home data={data} />;
 }
+
+export const getStaticProps = async () => {
+  const raw = await fetch(config.url + config.defaultSlug);
+  const json = await raw.json();
+  const { attributes } = json.data[0];
+  const data = mapData([attributes]);
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+Index.propTypes = {
+  data: P.array,
+};
