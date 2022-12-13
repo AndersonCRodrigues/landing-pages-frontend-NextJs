3638f507e908ffc6c18965583f0f0e2a8089ffe6
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-13 12:04:36 -0300

    add global style ao _app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,11 +2,13 @@ import P from 'prop-types';
 import { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../styles/theme';
+import { GlobalStyles } from '../styles/global-styles';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
+import {Home } from '../templates/Home';
 
-export default function Home() {
-  return <h1>Oi</h1>;
+export default function Index() {
+  return <Home />;
 }
