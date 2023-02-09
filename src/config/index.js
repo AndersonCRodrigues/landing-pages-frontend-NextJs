7ae65c8f7bbe3f82f0197edc692c3e65511e70cc
Anderson Rodrigues <anderson.c.rodrigues@hotmail.com>
2023-02-09 12:31:45 -0300

    [feat] altera endPoint

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
 export default {
-  url: 'http://localhost:1337/api/pages/?filter[slug]=',
+  url: 'https://landing-pages-plea.onrender.com/api/pages/?filter[slug]=',
   siteName: 'Anderson C Rodrigues',
   defaultSlug: 'landing-page',
 };
+
+const url = 'http://localhost:1337/api/pages/?filter[slug]=';
